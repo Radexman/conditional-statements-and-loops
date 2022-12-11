4a066402cd7e1cb05077cef21b00759de2a2e76f
Radosław Siek <106437063+Radexman@users.noreply.github.com>
2022-12-11 17:00:14 +0100

    12.11

--- main.js
+++ main.js
@@ -6,7 +6,7 @@
 'Hello' == 'Hello' // true
 'Hello' == 'Bye' // false
 
-//  === identical to, checks values nad data types
+//  === identical to, checks values and data types
 '3' === 3 // false
 '3' === '3' // true
 
@@ -43,7 +43,7 @@
 
 // Logical Operators
 
-// && Logical And both, expressions have to be true
+// && Logical And, both expressions have to be true
 
 true && true // true
 true && false // false
@@ -62,72 +62,85 @@ false || false // false
 !true // false
 !false // true
 
-// IF keyword checks if the expression is true and runs code in the block below
-
-let score = 49;
-
-if (score >= 50) {
-    console.log(`You have ${score} points. You Passed!`);
-} else {
-    console.log(`You have ${score} points. Take the test again`);
-}
-
 // Switch Statement
 
 let msgEl = document.querySelector('#msg');
 let msg;
-let level = 69;
+let level = 1;
 
 switch(level) {
     case 1:
-        msg = 'Good luck on first test';
+        msg = 'Good luck on first test!';
         break;
 
     case 2:
-        msg = 'Good luck on second test';
+        msg = 'Good luck on second test!';
         break;
 
     case 3:
-        msg = 'Good luck on last test';
+        msg = 'Good luck on the last test!';
         break;
 
     default:
-        msg = 'Good luck on test!';
-        break;
+        msg = 'Good luck on the test';
 }
+
+// IF keyword checks if the expression is true and runs code in the block below
+
+let pointsEl = document.querySelector('#points');
+let score = 62;
+let feedback;
+
+if (score >= 50) {
+    feedback = `You passed! You have ${score} points.`;
+} else {
+    feedback = `Take the test again. You have ${score} points.`;
+}
+
+pointsEl.textContent = feedback;
 
 msgEl.textContent = msg;
 
+let currentDay = new Date().getDay();
 let date = document.querySelector('#date');
 let day;
+let days = [
+    'Niedziala',
+    'Poniedziałek',
+    'Wtorek',
+    'Środa',
+    'Czwartek',
+    'Piątek',
+    'Sobota'
+]
 
-switch(new Date().getDay()) {
+switch(currentDay) {
     case 0:
-        day = 'Sunday';
+        day = days[0];
         break;
 
     case 1:
-        day = 'Monday';
+        day = days[1];
         break;
 
     case 2:
-        day = 'Tuesday';
+        day = days[2];
         break;
 
     case 3:
-        day = 'Wednesday';
+        day = days[3];
         break;
 
     case 4:
-        day = 'Thursday';
+        day = days[4];
         break;
 
     case 5:
-        day = 'Friday';
+        day = days[5];
         break;
 
     case 6:
-        day = 'Saturday';
+        day = days[6];
         break;
 }
 
@@ -136,20 +149,63 @@ date.textContent+= day;
 let wkd = document.querySelector('#wkd');
 let weekend;
 
-switch(new Date().getDay()) {
+switch(currentDay) {
+    case 0:
+    case 6:
+        weekend = 'It is weekend';
+        break;
+
     case 4:
     case 5:
-        weekend = 'Soon it is weekend';
+        weekend = 'Weekend is soon';
         break;
 
-    case 6:
-    case 0:
-        weekend =  'It is weekend';
-        break;
-
-    default: 
-        weekend = 'Looking forward to the weekend';
-        break;
+    default:
+        weekend = 'Weekend is not soon';
 }
 
-wkd.textContent+= weekend;
+wkd.textContent = weekend;
+
+// =============== Loops =============== //
+
+// Loop checks if statement is true and if it is loop executes code block
+// Statement is checked again and the code is executed until statement is false
+// There are three mos common usef loops: FOR, WHILE, DO-WHILE;
+// Declaring variables in global scope saves more memory
+
+// FOR executes the code block specified number of times and is the mos common loop
+
+// let scores = [24, 32, 17];
+// let arrayLenght = score.length;
+// let roundNumber = 0;
+// let roundMessage;
+// let i;
+// let roundEl = document.querySelector('#round');
+
+// for (i = 0; i < arrayLenght; i++) {
+//     roundNumber = (i + 1);
+//     console.log(`Runda ${roundNumber}: ${scores[i]}`);
+// }
+
+
+// WHILE if you don't know how many times loop has to run
+
+// let i = 1;
+// let message = '';
+
+// while (i < 10) {
+//     message+= i + ' x 5' + (i * 5);
+//     i++;
+// }
+
+// console.log(message);
+
+// DO-WHILE if you don't know how many times loop hus to run but it runs at least once
+
+// let i = 1;
+// let message = '';
+
+// do {
+//     message+= i + ' x 5 = ' + (i * 5);
+//     i++;
+// } while (i < 1);
